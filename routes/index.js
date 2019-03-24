f839c78437a7d491f8efe6aa4a72cdac45a390c9
akruft <akruft@ilstu.edu>
2019-03-23 19:30:44 -0500

    finished tutorial

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const { body, validationResult } = require('express-validator/check');
 const router = express.Router();
+const Registration = mongoose.model('Registration');
+
 router.get('/', (req, res) => {
     res.render('form', { title: 'Registration form' });
 });
@@ -16,8 +19,10 @@ body('email')
 (req, res) => {
     const errors = validationResult(req);
     if (errors.isEmpty()) {
-        console.log(req.body);
-    res.send('Thank you for your registration!');
+        const registration = new Registration(req.body);
+registration.save()
+.then(() => { res.send('Thank you for your registration!'); })
+.catch(() => { res.send('Sorry! Something went wrong.'); });
     } else {
         console.log(req.body);
     res.render('form', {
@@ -28,5 +33,11 @@ body('email')
     }
     }
     );
-    
+    router.get('/registrations', (req, res) => {
+        Registration.find()
+.then((registrations) => {
+res.render('index', { title: 'Listing registrations', registrations });
+})
+.catch(() => { res.send('Sorry! Something went wrong.'); });
+        });
 module.exports = router;
